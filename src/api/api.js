c1000dddc7b478c297389b5af1c267aa238b8c7b
Carlos Rauda <rauda.carlos6432@gmail.com>
2025-11-15 05:50:08 -0600

    fix url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'https://dentifia.onrender.com';
+export const API_URL = 'http://127.0.0.1:8000/';
 
 // Crear instancia de axios con configuración base
 const api = axios.create({
